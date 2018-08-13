7f2a7c99b3d413c52a3c7cc73cfaccfde9f49404
cml11 <cml11@CML11>
2018-08-13 17:38:06 -0300

    Criado linha para diretorio 2.1

---
+++ TesteDiretorio2.1/NovoDiretorio2ponto1/NovoDiretorio2ponto1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NovoDiretorio2ponto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Este é o diretorio 2.1 dentro do diretorio 2");
+            Console.ReadKey();
+        }
+    }
+}
